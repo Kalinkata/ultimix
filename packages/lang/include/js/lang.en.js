580921d0e2ab97a76d70ed807674981cc02c4149
Dodonov Alexey <alexey@dodonov.pro>
2012-04-13 16:35:01 +0400

    Clean temporary files

--- packages/lang/include/js/lang.en.js
+++
@@ -1,6 +0,0 @@
-if( !ultimix.lang.Strings[ ultimix.lang.Locale ][ 'name_ru' ] ) ultimix.lang.Strings[ ultimix.lang.Locale ][ 'name_ru' ] = {};
-ultimix.lang.Strings[ ultimix.lang.Locale ][ 'name_ru' ][ 'default' ] = 'rus';
-if( !ultimix.lang.Strings[ ultimix.lang.Locale ][ 'name_en' ] ) ultimix.lang.Strings[ ultimix.lang.Locale ][ 'name_en' ] = {};
-ultimix.lang.Strings[ ultimix.lang.Locale ][ 'name_en' ][ 'default' ] = 'eng';
-if( !ultimix.lang.Strings[ ultimix.lang.Locale ][ 'name_jp' ] ) ultimix.lang.Strings[ ultimix.lang.Locale ][ 'name_jp' ] = {};
-ultimix.lang.Strings[ ultimix.lang.Locale ][ 'name_jp' ][ 'default' ] = 'jap';
