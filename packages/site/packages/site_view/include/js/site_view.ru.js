9f28c67c6278fec7d660754702b5b5b46adb604e
Dodonov Alexey <alexey@dodonov.pro>
2012-04-14 16:53:08 +0400

    Cleanup

--- packages/site/packages/site_view/include/js/site_view.ru.js
+++
@@ -1,6 +0,0 @@
-if( !ultimix.lang.Strings[ ultimix.lang.Locale ][ 'site_manager' ] ) ultimix.lang.Strings[ ultimix.lang.Locale ][ 'site_manager' ] = {};
-ultimix.lang.Strings[ ultimix.lang.Locale ][ 'site_manager' ][ 'default' ] = 'Сайты';
-if( !ultimix.lang.Strings[ ultimix.lang.Locale ][ 'domain' ] ) ultimix.lang.Strings[ ultimix.lang.Locale ][ 'domain' ] = {};
-ultimix.lang.Strings[ ultimix.lang.Locale ][ 'domain' ][ 'default' ] = 'Домен';
-if( !ultimix.lang.Strings[ ultimix.lang.Locale ][ 'comment' ] ) ultimix.lang.Strings[ ultimix.lang.Locale ][ 'comment' ] = {};
-ultimix.lang.Strings[ ultimix.lang.Locale ][ 'comment' ][ 'default' ] = 'Комментарий';
