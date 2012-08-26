692222f3c23198ac63428487235b6dab0aff0c61
Dodonov Alexey <alexey@dodonov.pro>
2012-08-26 15:25:56 +0400

    Refactoring changes

---
+++ packages/settings/packages/settings_view/include/js/settings_view.js
@@ -0,0 +1,128 @@
+/**
+*	Global namespace.
+*
+*	@author Dodonov A.A.
+*/
+if( !ultimix )
+{
+	ultimix = {};
+}
+
+/**
+*	Local namespace.
+*
+*	@author Dodonov A.A.
+*/
+if( !ultimix.settings )
+{
+	ultimix.settings = {};
+}
+
+/**
+*	Function sets list view options.
+*
+*	@param ViewOptions - Extra view generation options.
+*
+*	@return View options.
+*
+*	@author Dodonov A.A.
+*/
+ultimix.settings.set_default_options = function( ViewOptions )
+{
+	if( !ViewOptions )
+	{
+		ViewOptions = {};
+	}
+
+	ViewOptions.meta = ViewOptions.meta ? ViewOptions.meta : 'meta_settings_list';
+	ViewOptions.package_name = ViewOptions.package_name ? ViewOptions.package_name : 'settings::settings_view';
+	ViewOptions.paging_require_form = ViewOptions.paging_require_form ? ViewOptions.paging_require_form : '0';
+	ViewOptions.add_hidden_fields = ViewOptions.add_hidden_fields ? ViewOptions.add_hidden_fields : '0';
+
+	return( ViewOptions );
+}
+
+/**
+*	Function returns list view.
+*
+*	@param Functions - Functions to process success and error events.
+*
+*	@param ViewOptions - Extra view generation options.
+*
+*	@author Dodonov A.A.
+*/
+ultimix.settings.get_list_form = function( Fuctions , ViewOptions )
+{
+	if( !Fuctions )
+	{
+		Fuctions = {};
+	}
+
+	ViewOptions = ultimix.settings.set_default_options( ViewOptions );
+
+	ultimix.ajax_gate.direct_view( ViewOptions , Fuctions );
+}
+
+/**
+*	Function returns list view.
+*
+*	@param Functions - Functions to process success and error events.
+*
+*	@param Header - List header template file name.
+*
+*	@param Item - List item template file name.
+*
+*	@param Footer - List footer template file name.
+*
+*	@param ViewOptions - Extra view generation options.
+*
+*	@author Dodonov A.A.
+*/
+ultimix.settings.get_custom_list_form = function( Fuctions , Header , Item , Footer , ViewOptions )
+{
+	if( !Fuctions )
+	{
+		Fuctions = {};
+	}
+
+	ViewOptions = ultimix.settings.set_default_options( ViewOptions );
+
+	ViewOptions.header = Header ? Header : 'settings_header.tpl';
+	ViewOptions.item = Item ? Item : 'settings_item.tpl';
+	ViewOptions.footer = Footer ? Footer : 'settings_footer.tpl';
+
+	ultimix.ajax_gate.direct_view( ViewOptions , Fuctions );
+}
+
+/**
+*	Function deletes record.
+*
+*	@param Id - Record id.
+*
+*	@param DataSelector - Data selector.
+*
+*	@author Dodonov A.A.
+*/
+ultimix.settings.delete = function( Id , DataSelector )
+{
+	//TODO: refactor all delete functions with a single method
+	ultimix.std_dialogs.QuestionMessageBox( 'are_you_shure' , 
+		function( Result )
+		{
+			if( Result == ultimix.std_dialogs.MB_YES )
+			{
+				var			ProgressDialogId = ultimix.std_dialogs.SimpleWaitingMessageBox();
+
+				ultimix.ajax_gate.direct_controller( 
+					{ 
+						'package_name' : 'settings::settings_controller' , 
+						'settings_context_action' : 'delete_record' , 
+						'settings_action' : 'delete_record' , 'settings_record_id' : Id , 
+						'meta' : 'meta_delete_settings'
+					} , 
+					{ 'success' :  ultimix.ajax_gate.succes_delete_function( DataSelector , ProgressDialogId ) } , {}
+				);
+			}
+		}
+	)
+}
